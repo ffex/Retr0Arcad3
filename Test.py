747cc7c2554cb5b12741aacce4c6c4b858f150b9
ffex <kurogan21@gmail.com>
2017-12-02 09:09:22 +0100

    class version of test

--- Test.py
+++ Test.py
@@ -2,36 +2,35 @@
 import RPi.GPIO as GPIO
 
 
-testPin=11
 
-def setup():
-    GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
+class Test:
+    def __init__(self,pin=11):
+        self._testPin=pin
+        self.setup()
+        self.addEvent()
 
-    GPIO.setup(testPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)    # Set BtnPin's mode is input, and pull up to high level(3.3V)
-        
+    def setup(self):
+        GPIO.setmode(GPIO.BOARD)       # Numbers GPIOs by physical location
 
-def keyPress(ev=None):
-    if GPIO.input(ev)==GPIO.LOW:
-        print('Down: Push!')
+        GPIO.setup(self._testPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)    # Set BtnPin's mode is input, and pull up to high level(3.3V)
 
-    else:
-        print('Up: Push!')
 
-        
-	
-def loop():
+    def keyPress(self,ev=None):
+        if GPIO.input(ev)==GPIO.LOW:
+            print('Down: Push!')
 
-    GPIO.add_event_detect(testPin, GPIO.BOTH, callback=keyPress,bouncetime=50) # wait for falling
-#        GPIO.add_event_detect(pin, GPIO.RISING, callback=keyUp,bouncetime=250) # wait for falling
-    while True:
-        pass   # Don't do anything
+        else:
+            print('Up: Push!')
 
-def destroy():
-	GPIO.cleanup()                     # Release resource
 
-if __name__ == '__main__':     # Program start from here
-    setup()
-    try:
-        loop()
-    except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
-        destroy()
+
+    def addEvent(self):
+
+        GPIO.add_event_detect(self._testPin, GPIO.BOTH, callback=keyPress,bouncetime=50) # wait for falling
+    #        GPIO.add_event_detect(pin, GPIO.RISING, callback=keyUp,bouncetime=250) # wait for falling
+    #    while True:
+    #        pass   # Don't do anything
+
+    def destroy(self):
+    	GPIO.cleanup()                     # Release resource
+
