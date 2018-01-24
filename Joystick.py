b5f387ee673edcc34f3b7b809cb64fd04a5f4ade
ffex <kurogan21@gmail.com>
2018-01-24 22:26:15 +0100

    split_joy code
    code for testing the start 3 device (p1,p2 and button mcp)

--- Joystick.py
+++ Joystick.py
@@ -51,52 +51,6 @@ class joystick:
     def destroy(self):
     	GPIO.cleanup()                     # Release resource
 
-class joystickMCP:
-    def __init__(self, ppins, keys, name):
-        self._name = name
-        self._pins = {}
-        self._prevStatus = {}
-        self.ui=""
-        self._mcp= MCP23017()
-        self.active=True
-        for x in range(len(ppins)):
-            self._pins[ppins[x]] = keys[x]
-            self._prevStatus[ppins[x]]=True
-        os.system("sudo modprobe uinput")
-        self.ui = UInput({e.EV_KEY:self._pins.values()},name="retrogame",bustype=e.BUS_USB)
-        self.setup()
-        self.loop()
 
-
-    def setup(self):
-        #GPIO.setmode(GPIO.BOARD)  # Numbers GPIOs by physical location
-        #self._mcp = MCP23017()
-        #self._mcp.setup(8,1)
-        #self._mcp.pullup(8,True)
-
-        for pin in self._pins.keys():
-            self._mcp.setup(pin, GPIO.IN)#tutti i pin in input
-            self._mcp.pullup(pin,True)#lì setto a 3.3v
-
-    def checkKeyPress(self, ev=None):
-        if self._mcp.input(ev) != self._prevStatus[ev]:
-            if self._mcp.input(ev) == GPIO.LOW:
-              #  print('Down: Push!')
-                self.ui.write(e.EV_KEY, self._pins[ev], 1)
-            else:
-               # print('Up: Push!')
-                self.ui.write(e.EV_KEY, self._pins[ev], 0)
-            self._prevStatus[ev] = self._mcp.input(ev)
-        self.ui.syn()
-
-
-    def loop(self):
-        while self.active:
-            for pin in self._pins.keys():
-                self.checkKeyPress(pin)
-
-
-    def destroy(self):
-        self.active=False
 
 
